323d1bb1a0429746c3f0fc5bc6de00c57de8bc89
Moises Guerola <daw.moisesguerola@gmail.com>
2022-09-30 13:05:44 +0200

    Directions from Snake

--- src/class/SnakeBody.js
+++ src/class/SnakeBody.js
@@ -8,6 +8,14 @@ class SnakeBody {
         this.direction
         this.oldDirection
         this.sprite = new Image()
+        this.sprites = {
+            LEFT_RIGTH: 'src/assets/img/snakeBody_left.png',
+            UP_RIGHT: "src/assets/img/snakeBody_rotateUpRight.png",
+            UP_LEFT: 'src/assets/img/snakeBody_rotateUpLeft.png',
+            DOWN_LEFT: "src/assets/img/snakeBody_rotateDownLeft.png",
+            DOWN_RIGTH: "src/assets/img/snakeBody_rotateDownRight.png",
+            DOWN_UP: 'src/assets/img/snakeBody_down.png',
+        }
     }
 
     draw(headDirection) {
@@ -44,71 +52,84 @@ class SnakeBody {
 
     checkPart(nextMove, player, nextBodyPart) {
         // ANCHOR: Left Site
-        if (player.x == nextMove.x && nextMove.x < this.x) {
-            // NOTE U Movement Up from Left
-            if (this.y > player.y) {
-                this.sprite.src = 'src/assets/img/snakeBody_rotateUpRight.png'
-                if (typeof nextBodyPart != "undefined") {
-                    nextBodyPart.sprite.src = 'src/assets/img/snakeBody_rotateDownLeft.png'
-                }
-            // NOTE ⋂ Movement from Left
-            } else if (this.y < player.y) {
-                this.sprite.src = 'src/assets/img/snakeBody_rotateDownRight.png'
-                if (typeof nextBodyPart != "undefined") {
-                    nextBodyPart.sprite.src = 'src/assets/img/snakeBody_rotateUpLeft.png'
-                }
-            }
-        // ANCHOR Right Site
-        } else if (player.x == nextMove.x && nextMove.x > this.x) {
-            // NOTE U ⊐ Movement from Left
-            if (this.y > player.y) {
-                this.sprite.src = 'src/assets/img/snakeBody_rotateDownLeft.png'
-                if (typeof nextBodyPart != "undefined") {
-                    nextBodyPart.sprite.src = 'src/assets/img/snakeBody_rotateUpRight.png'
-                }
-            // NOTE ⋂⊐ Movement from Left
-            } else if (this.y < player.y) {
-                this.sprite.src = 'src/assets/img/snakeBody_rotateUpLeft.png'
-                if (typeof nextBodyPart != "undefined") {
-                    nextBodyPart.sprite.src = 'src/assets/img/snakeBody_rotateDownRight.png'
-                }
-            }
-        // ANCHOR Right and Left Movement    
-        } else if (
-            player.x > nextMove.x && nextMove.x > this.x || 
-            player.x < nextMove.x && nextMove.x < this.x
-        ) {
-            this.sprite.src = 'src/assets/img/snakeBody_left.png'
-        // ANCHOR Up Down Movement
-        } else if (
-            player.y > nextMove.y && nextMove.y > this.y ||
-            player.y < nextMove.y && nextMove.y < this.y
-        ) {
-            this.sprite.src = 'src/assets/img/snakeBody_down.png'
-        }
-        
-        
-        // if (
-        //     this.y < nextMove.y && nextMove.y > this.y &&
-        //     nextMove.x > this.x && this.x < nextMove.x      
-        //     ) {
-        //     console.log('INSIDE');
-        //     this.sprite.src = 'src/assets/img/snakeBody_rotateDownLeft.png'
+        // if (player.x == nextMove.x && nextMove.x < this.x) {
+        //     // NOTE U Movement Up from Left
+        //     if (this.y > player.y) {
+        //         this.sprite.src = 'src/assets/img/snakeBody_rotateUpRight.png'
+        //         if (typeof nextBodyPart != "undefined" && player.x < this.x) {
+        //             console.log('A');
+        //             // nextBodyPart.sprite.src = "src/assets/img/snakeBody_rotateUpLeft.png"
+        //         } else if (typeof nextBodyPart != "undefined") {
+        //             nextBodyPart.sprite.src = 'src/assets/img/snakeBody_rotateDownLeft.png'
+        //         }
+        //     // NOTE ⋂ Movement from Left
+        //     } else if (this.y < player.y) {
+        //         this.sprite.src = 'src/assets/img/snakeBody_rotateDownRight.png'
+        //         if (typeof nextBodyPart != "undefined") {
+        //             nextBodyPart.sprite.src = 'src/assets/img/snakeBody_rotateUpLeft.png'
+        //         }
+        //     }
+        // // ANCHOR Right Site
+        // } else if (player.x == nextMove.x && nextMove.x > this.x) {
+        //     // NOTE U ⊐ Movement from Left
+        //     if (this.y > player.y) {
+        //         this.sprite.src = 'src/assets/img/snakeBody_rotateDownLeft.png'
+        //         if (typeof nextBodyPart != "undefined") {
+        //             nextBodyPart.sprite.src = 'src/assets/img/snakeBody_rotateUpRight.png'
+        //         }
+        //     // NOTE ⋂⊐ Movement from Left
+        //     } else if (this.y < player.y) {
+        //         this.sprite.src = 'src/assets/img/snakeBody_rotateUpLeft.png'
+        //         if (typeof nextBodyPart != "undefined") {
+        //             nextBodyPart.sprite.src = 'src/assets/img/snakeBody_rotateDownRight.png'
+        //         }
+        //     }
+        // // ANCHOR Right and Left Movement    
         // } else if (
-        //     // Left - Right
-        //     this.x < nextMove.x && nextMove.x > this.x &&
-        //     this.y == nextMove.y || this.x > nextMove.x && nextMove.x < this.x &&
-        //     this.y == nextMove.y
+        //     player.x > nextMove.x && nextMove.x > this.x || 
+        //     player.x < nextMove.x && nextMove.x < this.x
         // ) {
         //     this.sprite.src = 'src/assets/img/snakeBody_left.png'
+        // // ANCHOR Up Down Movement
         // } else if (
-        //     // Up - Down
-        //     this.y > nextMove.y && nextMove.y < this.y &&
-        //     this.x == nextMove.x || this.y < nextMove.y && nextMove.y > this.y &&
-        //     this.x == nextMove.x
+        //     player.y > nextMove.y && nextMove.y > this.y ||
+        //     player.y < nextMove.y && nextMove.y < this.y
         // ) {
         //     this.sprite.src = 'src/assets/img/snakeBody_down.png'
         // }
+        
+        // FIXME RIGHT LEFT MOVEMENT
+        if (nextMove.x > this.x || nextMove.x < this.x) {
+            this.sprite.src = this.sprites.LEFT_RIGTH
+            if (player.y > this.y) {
+                if (player.x > this.x && player.x == nextMove.x) {
+                    this.sprite.src = this.sprites.UP_LEFT
+                } else if (player.x < this.x && player.x == nextMove.x) {
+                    this.sprite.src = this.sprites.DOWN_RIGTH
+                }
+            } else if (player.y < this.y) {
+                if (player.x > this.x && player.x == nextMove.x) {
+                    this.sprite.src = this.sprites.DOWN_LEFT
+                } else if (player.x < this.x && player.x == nextMove.x) {
+                    this.sprite.src = this.sprites.UP_RIGHT
+                }
+            }
+        } else if (nextMove.y > this.y || nextMove.y < this.y) {
+            this.sprite.src = this.sprites.DOWN_UP
+            if (player.x > this.x) {
+                if (player.y > this.y && player.y == nextMove.y) {
+                    this.sprite.src = this.sprites.UP_RIGHT
+                } else if (player.y < this.y && player.y == nextMove.y) {
+                    this.sprite.src = this.sprites.DOWN_RIGTH
+                }
+            } else if (player.x < this.x) {
+                if (player.y > this.y && player.y == nextMove.y) {
+                    this.sprite.src = this.sprites.UP_RIGHT
+                } else if (player.y < this.y && player.y == nextMove.y) {
+                    this.sprite.src = this.sprites.DOWN_RIGTH
+                }
+            }
+        }
 
 
         this.draw()
